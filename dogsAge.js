acc6fa5e48efc7607c8dc11919c84c726ce00f6d
Yevheniia <steliva2003@gmail.com>
2022-04-19 17:11:13 +0200

    added prompt and document.write

--- dogsAge.js
+++ dogsAge.js
@@ -1,4 +1,4 @@
-const myAge = 1;
+const myAge = prompt("Please give your age");
 //it equal to your age as a number.
 let earlyYears = 2;
 // dog's first 2 years
@@ -9,6 +9,6 @@ laterYears *= 4;
 //to calculate the number of dog years accounted for by your later years
 var myAgeInDogYears = earlyYears + laterYears;
 var myName = "Jane".toLowerCase();
-console.log(
+document.write(
   `My name is ${myName}. I am ${myAge}years old in human years which is ${myAgeInDogYears} years old in dog years.`
 );
